350a509591a972f45aa4af4d3ba044d4d1642523
reidsm <45023406+reidsm@users.noreply.github.com>
2019-07-15 15:57:45 -0700

    Add files via upload

---
+++ Battleship.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Battleship {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("tacos");
+        list.add("hamburgers");
+        list.add("pizza");
+        list.add("almonds");
+        System.out.println(list);
+        System.out.println(maxLength(list));
+    }
+    public static int maxLength(ArrayList<String> list) {
+        ArrayList<Integer> listLength = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++){
+            if(list.size() == 0){
+                return 0;
+            } else {
+                listLength.add(list.get(i).length());
+                Collections.sort(listLength, Collections.reverseOrder());
+            }
+        }
+        return(listLength.get(0));
+    }
+}
+
+
+/*
+int [][] numbers = new int [3][4];
+            for (int r = 0; r < numbers.length; r++){
+                for (int c = 0; c < numbers[0].length; c++){
+                    numbers [r][c] = r + c;
+ */
