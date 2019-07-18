ffb02c27c98c2242ba9d2b5d33709e8c966eb35d
reidsm <45023406+reidsm@users.noreply.github.com>
2019-07-17 20:21:38 -0700

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -3,11 +3,17 @@ public class Fraction {
      private int numerator;
      private int denominator;
 
+    /*the first constructor for when we provide the constructor two parameters
+    it checks the setFractionValues method because the assignment said to, but I'm not sure
+    what it really does given the ironclad nature of the validation method in the main class*/
+
     public Fraction(int num, int den){
         this.numerator = num;
         this.denominator = den;
         setFractionValues();
     }
+
+    /*the constructor for an integer input, sets the denominator to 1 so the math methods don't get weird*/
 
     public Fraction(int num){
         this.numerator = num;
@@ -15,16 +21,23 @@ public class Fraction {
         setFractionValues();
     }
 
+    /*and finally sets the fraction to 0/1 for when the numerator is 0 or there are no parameters*/
+
     public Fraction(){
         this.numerator = 0;
         this.denominator = 1;
         setFractionValues();
     }
 
+    /*pretty self explanatory, makes the numerator and denominator into a string*/
+
     public String toString(){
         String frac = numerator + "/" + denominator;
         return frac;
     }
+
+    /*tbh not sure if this is needed but I made it early on because the assignment said to and now I'm afraid to
+    touch it lol*/
 
     public void setFractionValues(){
         if(numerator < 0 && denominator < 0){
@@ -38,14 +51,22 @@ public class Fraction {
         }
     }
 
+    /*basically goes through the math of adding two fractions, assumes that bases will be different
+    * and so it always converts the base no matter what since the toLowestTerms function will auto-reduce the answer anyway*/
+
     public Fraction add(Fraction secondFraction){
         int firstConvertedNumerator = this.denominator * secondFraction.numerator;
         int secondConvertedNumerator = this.numerator * secondFraction.denominator;
-        int newNumerator = firstConvertedNumerator - secondConvertedNumerator;
+        int newNumerator = firstConvertedNumerator + secondConvertedNumerator;
         int newDenominator = this.denominator * secondFraction.denominator;
         Fraction newFraction = new Fraction(newNumerator, newDenominator);
         return newFraction;
     }
+
+    /*Originally I had this as basically a copy of the addition method but with a '-' instead of the
+    * '+'. Then when I tested 1/4 - 1/4 I got -8/16 or something crazy, so I had to add conditional
+    logic that worked with the absolute values to get the math to work out. Probably a better way of doing this, could
+    have a mistake*/
 
     public Fraction subtract(Fraction secondFraction){
         int firstConvertedNumerator = Math.abs(this.denominator * secondFraction.numerator);
@@ -76,6 +97,9 @@ public class Fraction {
         return newFraction;
     }
 
+    /*had to google this to remember how to divide fractions - gotta take the recirpocal! 7th grade me would have been
+    disappointed*/
+
     public Fraction divide(Fraction secondFraction){
         int reciprocalNumerator = secondFraction.denominator;
         int reciprocalDenominator = secondFraction.numerator;
@@ -93,12 +117,16 @@ public class Fraction {
         return this.denominator;
     }
 
+    /*don't try dividing two ints, it gives weird answers. convert the ints to doubles first*/
+
     public double toDouble(){
         double newNumerator = (double)numerator;
         double newDenominator = (double)denominator;
         double newDouble = newNumerator/newDenominator;
         return newDouble;
     }
+
+    /*Logically this converts two fractions to doubles and checks if they are the same double*/
 
     public boolean equals(Fraction otherFraction){
         double newNumerator = (double)this.numerator;
@@ -112,6 +140,9 @@ public class Fraction {
         }return false;
     }
 
+    /*passes numerator and denominator ints to the gcd function to find the gcd, divides both the numerator and the
+    denominator by the gcd, and then returns the new, reduced fraction*/
+
     public Fraction toLowestTerms(){
         int gcd = gcd(this.numerator, this.denominator);
         int newNumerator = numerator/gcd;
@@ -119,6 +150,8 @@ public class Fraction {
         Fraction newFraction = new Fraction(newNumerator, newDenominator);
         return newFraction;
     }
+
+    /*honestly I googled this, don't 100% understand how this works*/
 
     public static int gcd(int firstNumber, int secondNumber){
         if(secondNumber == 0){
--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 import java.util.*;
 public class Main {
+
+    /*The main class on the client side asks for the operation by calling the getOperation method,
+    then gathers two strings that are meant to be fractions by calling the input method (which itself then
+    calls the validation method). Then it passes only valid strings to the fraction converter function,
+    and finally calls fraction-specific methods depending on the type of input stored in the operation variable*/
+
     public static void main(String args[]) {
         char operation = getOperation();
         String firstFractionString = input();
@@ -26,6 +32,10 @@ public class Main {
         }
     }
 
+    /*Not too hard here, we know it has to be one of 7 characters so if it doesn't
+    match one then we call the method again to start over and print a little "try again"
+    message*/
+
     public static char getOperation(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your operation: ");
@@ -50,6 +60,11 @@ public class Main {
         }
     }
 
+    /*This one is harder because the fraction string has many, many more valid inputs than
+   the operation method. So it calls the validFunction method for the boolean and basically
+   if the fraction string is not valid then the input method prints little "try again" method
+   and then uses recursion to re-prompt the user for a valid string*/
+
     public static String input(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your fraction: ");
@@ -59,6 +74,12 @@ public class Main {
             userFraction = input();
         }return userFraction;
     }
+
+    /*I found this to be the second most challenging part of the program. In fact, this is not perfect and is
+    incomplete but I need to move on now. Basically this method checks to see if the fraction provided is in the
+    'a/b' format by looking for the character '/'. If so, it makes sure that the denominator is not 0.
+    The method then makes sure there are no letters using a regex, and then starts making sure there's no other
+    misc. punctuation (though I didn't include all of it because I had to restart this two times and enough is enough)*/
 
     public static boolean validFraction(String userFraction){
         if(userFraction.contains(Character.toString('/'))){
@@ -70,7 +91,7 @@ public class Main {
             }else if(den < 0){
                 return false;
             }
-        }else if(userFraction.matches(".*[a-zA-Z]+.*")){
+        }else if(userFraction.matches(".*[a-zA-Z]+.*")){//regex: "if this contains one or more letters then return true, and thus return a false condition for being a valid string"
             return false;
         }else if(userFraction.contains(Character.toString(' '))){
             return false;
@@ -85,6 +106,24 @@ public class Main {
         }return true;
     }
 
+    /*The hardest part of the program. Here I learned that you can indeed have more than one return statement in
+    a function as long as you don't try to refactor longer bits of code outside of the function and every single if/else
+    clause has a return statement. I also had to start this over many times due to misunderstandings about domains and
+    such (num and den not showing up outside of conditional and loop statements)
+
+    Logically, it first checks to see basically if the fraction is not an integer (do you have a '/' in you?) If so,
+    it checks to see if the numerator is 0, and if it is, it makes the fraction 0/1 by calling the parameterless constructor
+    which I'll get into later. Otherwise, it uses the regular constructor since we know from the validFraction method that the
+    denominator will not be 0.
+
+    It took me a few attempts before I figured out how to use the fractionString.split and then parse strings to int. Before this I
+    tried manipulating strings in really convoluted ways that got in each others way and it was a mess.
+
+     If the string doesn't have a '/' in it we can pretty safely assume that this is an integer by this point. If it starts with a '-' it's
+     a negative integer, so I made some more use of the Integer.parseInt method again.
+
+     There is a high chance that this has a mistake in it somewhere, but again, time to move on.*/
+
     public static Fraction getFraction(String fractionString){
         if(fractionString.contains(Character.toString('/'))){
             String[] fract = fractionString.split("/");
@@ -97,7 +136,7 @@ public class Main {
                 Fraction newFraction = new Fraction(num, den);
                 return newFraction;
             }
-        }else if(fractionString.matches("[0-9]+")){
+        }else if(fractionString.matches("[0-9]+")){ //If you contain at least one number and nothing else then return true (*I THINK, could be wrong so be careful)
             int num = Integer.parseInt(fractionString);
             Fraction newFraction = new Fraction(num);
             return newFraction;
